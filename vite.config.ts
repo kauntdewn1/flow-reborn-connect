1a8d511b0664a36af6ae47b895d4396709f36ea8
MyrnaClyde <114366115+kauntdewn1@users.noreply.github.com>
2025-05-07 01:05:52 -0300

    play-react

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('framer-motion')) return 'vendor_framer';
+            if (id.includes('@ton')) return 'vendor_ton';
+            return 'vendor';
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 800, // Aumenta a tolerância
+  }
 })
